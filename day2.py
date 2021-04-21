22113de55a75a19321b7e4e68637a3147b2d5abf
“1872134463” <1872134463@qq.com>
2021-04-21 17:25:22 +0800

    第二次作业

---
+++ day2.py
@@ -0,0 +1,191 @@
+
+date1 =1
+name1 ="羽绒服"
+price1 =253.6
+kucun1 =500
+xiaoshoul1 =10
+date2 ="2号"
+name2 ="牛仔裤"
+price2 =86.3
+kucun2 =600
+xiaoshoul2 =60
+date3 ="3号"
+name3 ="风衣"
+price3 =96.8
+kucun3 =335
+xiaoshoul3 =43
+date4 ="4号"
+name4 ="皮草"
+price4 =135.9
+kucun4 =855
+xiaoshoul4 =63
+date5 ="5号"
+name5 ="T恤"
+price5 =65.8
+kucun5 =632
+xiaoshoul5 =63
+date6 ="6号"
+name6 ="衬衫"
+price6 =49.3
+kucun6 =562
+xiaoshoul6 =120
+date7 ="7号"
+name7 ="牛仔裤"
+price7 =86.3
+kucun7 =600
+xiaoshoul7 =72
+date8 ="8号"
+name8 ="羽绒服"
+price8 =253.6
+kucun8 =500
+xiaoshoul8 =69
+date9 ="9号"
+name9 ="牛仔裤"
+price9 =86.3
+kucun9 =600
+xiaoshoul9 =35
+date10 ="10号"
+name10 ="羽绒服"
+price10 =253.6
+kucun10 =500
+xiaoshoul10 =140
+date11 ="11号"
+name11 ="牛仔裤"
+price11 =86.3
+kucun11 =600
+xiaoshoul11 =90
+date12 ="12号"
+name12 ="皮草"
+price12 =135.9
+kucun12 =855
+xiaoshoul12 =24
+date13 ="13号"
+name13 ="T恤"
+price13 =65.8
+kucun13 =632
+xiaoshoul13 =45
+date14 ="14号"
+name14 ="风衣"
+price14 =96.8
+kucun14 =335
+xiaoshoul14 =25
+date15 ="15号"
+name15 ="牛仔裤"
+price15 =86.3
+kucun15 =600
+xiaoshoul15 =60
+date16 ="16号"
+name16 ="T恤"
+price16 =65.8
+kucun16 =632
+xiaoshoul16 =129
+date17 ="17号"
+name17 ="羽绒服"
+price17 =253.6
+kucun17 =500
+xiaoshoul17 =10
+date18 ="18号"
+name18 ="风衣"
+price18 =96.8
+kucun18 =335
+xiaoshoul18 =43
+date19 ="19号"
+name19 ="T恤"
+price19 =65.8
+kucun19 =632
+xiaoshoul19 =63
+date20 ="20号"
+name20 ="牛仔裤"
+price20 =86.3
+kucun20 =600
+xiaoshoul20 =60
+date21 ="21号"
+name21 ="皮草"
+price21 =135.9
+kucun21 =855
+xiaoshoul21 =63
+date22 ="22号"
+name22 ="风衣"
+price22 =96.8
+kucun22 =335
+xiaoshoul22 =60
+date23 ="23号"
+name23 ="T恤"
+price23 =65.8
+kucun23 =632
+xiaoshoul23 =58
+date24 ="24号"
+name24 ="牛仔裤"
+price24 =86.3
+kucun24 =600
+xiaoshoul24 =140
+date25 ="25号"
+name25 ="T恤"
+price25 =65.8
+kucun25 =632
+xiaoshoul25 =63
+date26 ="26号"
+name26 ="风衣"
+price26 =96.8
+kucun26 =335
+xiaoshoul26 =43
+date27 ="27号"
+name27 ="皮草"
+price27 =135.9
+kucun27 =855
+xiaoshoul27 =57
+date28 ="28号"
+name28 ="羽绒服"
+price28 =253.6
+kucun28 =500
+xiaoshoul28 =10
+date29 ="29号"
+name29 ="T恤"
+price29 =65.8
+kucun29 =632
+xiaoshoul29 =63
+date30 ="30号"
+name30 ="风衣"
+price30 =96.8
+kucun30 =335
+xiaoshoul30 =78
+
+print("日期","\t\t","服装名称","\t\t","价格/件","\t\t","库存数量","\t\t","销售量/每日")
+print(date1,"\t\t",name1,"\t\t",price1,"\t\t",kucun1,"\t\t",xiaoshoul1)
+print(date2,"\t\t",name2,"\t\t",price2,"\t\t",kucun2,"\t\t",xiaoshoul2)
+print(date3,"\t\t",name3,"\t\t",price3,"\t\t",kucun3,"\t\t",xiaoshoul3)
+print(date4,"\t\t",name4,"\t\t",price4,"\t\t",kucun4,"\t\t",xiaoshoul4)
+print(date5,"\t\t",name5,"\t\t",price5,"\t\t",kucun5,"\t\t",xiaoshoul5)
+print(date6,"\t\t",name6,"\t\t",price6,"\t\t",kucun6,"\t\t",xiaoshoul6)
+print(date7,"\t\t",name7,"\t\t",price7,"\t\t",kucun7,"\t\t",xiaoshoul7)
+print(date8,"\t\t",name8,"\t\t",price8,"\t\t",kucun8,"\t\t",xiaoshoul8)
+print(date9,"\t\t",name9,"\t\t",price9,"\t\t",kucun9,"\t\t",xiaoshoul9)
+print(date10,"\t\t",name10,"\t\t",price10,"\t\t",kucun10,"\t\t",xiaoshoul10)
+print(date11,"\t\t",name11,"\t\t",price11,"\t\t",kucun11,"\t\t",xiaoshoul11)
+print(date12,"\t\t",name12,"\t\t",price12,"\t\t",kucun12,"\t\t",xiaoshoul12)
+print(date13,"\t\t",name13,"\t\t",price13,"\t\t",kucun13,"\t\t",xiaoshoul13)
+print(date14,"\t\t",name14,"\t\t",price14,"\t\t",kucun14,"\t\t",xiaoshoul14)
+print(date15,"\t\t",name15,"\t\t",price15,"\t\t",kucun15,"\t\t",xiaoshoul15)
+print(date16,"\t\t",name16,"\t\t",price16,"\t\t",kucun16,"\t\t",xiaoshoul16)
+print(date17,"\t\t",name17,"\t\t",price17,"\t\t",kucun17,"\t\t",xiaoshoul17)
+print(date18,"\t\t",name18,"\t\t",price18,"\t\t",kucun18,"\t\t",xiaoshoul18)
+print(date19,"\t\t",name19,"\t\t",price19,"\t\t",kucun19,"\t\t",xiaoshoul19)
+print(date20,"\t\t",name20,"\t\t",price20,"\t\t",kucun20,"\t\t",xiaoshoul20)
+print(date21,"\t\t",name21,"\t\t",price21,"\t\t",kucun21,"\t\t",xiaoshoul21)
+print(date22,"\t\t",name22,"\t\t",price22,"\t\t",kucun22,"\t\t",xiaoshoul22)
+print(date23,"\t\t",name23,"\t\t",price23,"\t\t",kucun23,"\t\t",xiaoshoul23)
+print(date24,"\t\t",name24,"\t\t",price24,"\t\t",kucun24,"\t\t",xiaoshoul24)
+print(date25,"\t\t",name25,"\t\t",price25,"\t\t",kucun25,"\t\t",xiaoshoul25)
+print(date26,"\t\t",name26,"\t\t",price26,"\t\t",kucun26,"\t\t",xiaoshoul26)
+print(date27,"\t\t",name27,"\t\t",price27,"\t\t",kucun27,"\t\t",xiaoshoul27)
+print(date28,"\t\t",name28,"\t\t",price28,"\t\t",kucun28,"\t\t",xiaoshoul28)
+print(date29,"\t\t",name29,"\t\t",price29,"\t\t",kucun29,"\t\t",xiaoshoul29)
+print(date30,"\t\t",name30,"\t\t",price30,"\t\t",kucun30,"\t\t",xiaoshoul30)
+print("总销售额:",(xiaoshoul1*price1+xiaoshoul2*price2+xiaoshoul3*price3+xiaoshoul4*price4+xiaoshoul5*price5+xiaoshoul6*price6+xiaoshoul7*price7+xiaoshoul8*price8+xiaoshoul9*price9+xiaoshoul10*price10+xiaoshoul11*price11+xiaoshoul12*price12+xiaoshoul13*price13+xiaoshoul14*price14+xiaoshoul15*price15+xiaoshoul16*price16+xiaoshoul17*price17+xiaoshoul18*price18+xiaoshoul19*price19+xiaoshoul20*price20+xiaoshoul21*price21+xiaoshoul22*price22+xiaoshoul23*price23+xiaoshoul24*price24+xiaoshoul25*price25+xiaoshoul26*price26+xiaoshoul27*price27+xiaoshoul28*price28+xiaoshoul29*price29+xiaoshoul30*price30))
+print("羽绒服销售占比：",((xiaoshoul1+xiaoshoul8+xiaoshoul10+xiaoshoul17+xiaoshoul28)*price1/(xiaoshoul1*price1+xiaoshoul2*price2+xiaoshoul3*price3+xiaoshoul4*price4+xiaoshoul5*price5+xiaoshoul6*price6+xiaoshoul7*price7+xiaoshoul8*price8+xiaoshoul9*price9+xiaoshoul10*price10+xiaoshoul11*price11+xiaoshoul12*price12+xiaoshoul13*price13+xiaoshoul14*price14+xiaoshoul15*price15+xiaoshoul16*price16+xiaoshoul17*price17+xiaoshoul18*price18+xiaoshoul19*price19+xiaoshoul20*price20+xiaoshoul21*price21+xiaoshoul22*price22+xiaoshoul23*price23+xiaoshoul24*price24+xiaoshoul25*price25+xiaoshoul26*price26+xiaoshoul27*price27+xiaoshoul28*price28+xiaoshoul29*price29+xiaoshoul30*price30)))
+print("牛仔裤销售占比：",((xiaoshoul2+xiaoshoul7+xiaoshoul9+xiaoshoul11+xiaoshoul15+xiaoshoul20+xiaoshoul24)*price2/(xiaoshoul1*price1+xiaoshoul2*price2+xiaoshoul3*price3+xiaoshoul4*price4+xiaoshoul5*price5+xiaoshoul6*price6+xiaoshoul7*price7+xiaoshoul8*price8+xiaoshoul9*price9+xiaoshoul10*price10+xiaoshoul11*price11+xiaoshoul12*price12+xiaoshoul13*price13+xiaoshoul14*price14+xiaoshoul15*price15+xiaoshoul16*price16+xiaoshoul17*price17+xiaoshoul18*price18+xiaoshoul19*price19+xiaoshoul20*price20+xiaoshoul21*price21+xiaoshoul22*price22+xiaoshoul23*price23+xiaoshoul24*price24+xiaoshoul25*price25+xiaoshoul26*price26+xiaoshoul27*price27+xiaoshoul28*price28+xiaoshoul29*price29+xiaoshoul30*price30)))
+print("风衣销售占比:",((xiaoshoul3+xiaoshoul14+xiaoshoul18+xiaoshoul22+xiaoshoul26+xiaoshoul30)*price3/(xiaoshoul1*price1+xiaoshoul2*price2+xiaoshoul3*price3+xiaoshoul4*price4+xiaoshoul5*price5+xiaoshoul6*price6+xiaoshoul7*price7+xiaoshoul8*price8+xiaoshoul9*price9+xiaoshoul10*price10+xiaoshoul11*price11+xiaoshoul12*price12+xiaoshoul13*price13+xiaoshoul14*price14+xiaoshoul15*price15+xiaoshoul16*price16+xiaoshoul17*price17+xiaoshoul18*price18+xiaoshoul19*price19+xiaoshoul20*price20+xiaoshoul21*price21+xiaoshoul22*price22+xiaoshoul23*price23+xiaoshoul24*price24+xiaoshoul25*price25+xiaoshoul26*price26+xiaoshoul27*price27+xiaoshoul28*price28+xiaoshoul29*price29+xiaoshoul30*price30)))
+print("皮草销售占比:",((xiaoshoul4+xiaoshoul12+xiaoshoul21+xiaoshoul27)*price4/(xiaoshoul1*price1+xiaoshoul2*price2+xiaoshoul3*price3+xiaoshoul4*price4+xiaoshoul5*price5+xiaoshoul6*price6+xiaoshoul7*price7+xiaoshoul8*price8+xiaoshoul9*price9+xiaoshoul10*price10+xiaoshoul11*price11+xiaoshoul12*price12+xiaoshoul13*price13+xiaoshoul14*price14+xiaoshoul15*price15+xiaoshoul16*price16+xiaoshoul17*price17+xiaoshoul18*price18+xiaoshoul19*price19+xiaoshoul20*price20+xiaoshoul21*price21+xiaoshoul22*price22+xiaoshoul23*price23+xiaoshoul24*price24+xiaoshoul25*price25+xiaoshoul26*price26+xiaoshoul27*price27+xiaoshoul28*price28+xiaoshoul29*price29+xiaoshoul30*price30)))
+print("T恤销售占比：",((xiaoshoul5+xiaoshoul13+xiaoshoul16+xiaoshoul19+xiaoshoul23+xiaoshoul25+xiaoshoul29)*price5/(xiaoshoul1*price1+xiaoshoul2*price2+xiaoshoul3*price3+xiaoshoul4*price4+xiaoshoul5*price5+xiaoshoul6*price6+xiaoshoul7*price7+xiaoshoul8*price8+xiaoshoul9*price9+xiaoshoul10*price10+xiaoshoul11*price11+xiaoshoul12*price12+xiaoshoul13*price13+xiaoshoul14*price14+xiaoshoul15*price15+xiaoshoul16*price16+xiaoshoul17*price17+xiaoshoul18*price18+xiaoshoul19*price19+xiaoshoul20*price20+xiaoshoul21*price21+xiaoshoul22*price22+xiaoshoul23*price23+xiaoshoul24*price24+xiaoshoul25*price25+xiaoshoul26*price26+xiaoshoul27*price27+xiaoshoul28*price28+xiaoshoul29*price29+xiaoshoul30*price30)))
+print("衬衫销售占比：",(xiaoshoul6*price6/(xiaoshoul1*price1+xiaoshoul2*price2+xiaoshoul3*price3+xiaoshoul4*price4+xiaoshoul5*price5+xiaoshoul6*price6+xiaoshoul7*price7+xiaoshoul8*price8+xiaoshoul9*price9+xiaoshoul10*price10+xiaoshoul11*price11+xiaoshoul12*price12+xiaoshoul13*price13+xiaoshoul14*price14+xiaoshoul15*price15+xiaoshoul16*price16+xiaoshoul17*price17+xiaoshoul18*price18+xiaoshoul19*price19+xiaoshoul20*price20+xiaoshoul21*price21+xiaoshoul22*price22+xiaoshoul23*price23+xiaoshoul24*price24+xiaoshoul25*price25+xiaoshoul26*price26+xiaoshoul27*price27+xiaoshoul28*price28+xiaoshoul29*price29+xiaoshoul30*price30)))
+print("每件衣服销售占比:",(xiaoshoul1+xiaoshoul2+xiaoshoul3+xiaoshoul4+xiaoshoul5+xiaoshoul6+xiaoshoul7+xiaoshoul8+xiaoshoul9+xiaoshoul10+xiaoshoul11+xiaoshoul12+xiaoshoul13+xiaoshoul14+xiaoshoul15+xiaoshoul16+xiaoshoul17+xiaoshoul18+xiaoshoul19+xiaoshoul20+xiaoshoul21+xiaoshoul22+xiaoshoul23+xiaoshoul24+xiaoshoul25+xiaoshoul26+xiaoshoul27+xiaoshoul28+xiaoshoul29+xiaoshoul30)/(kucun1+kucun2+kucun3+kucun4+kucun5+kucun6))
